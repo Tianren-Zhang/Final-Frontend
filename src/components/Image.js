42b8c6a870f8b4b3593d89c990531ea4031cde68
ztrrrrr <84163642+ztrrrrr@users.noreply.github.com>
2024-03-26 12:10:09 -0400

    Adjust the color

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ImageBlock = ({ imageUrl }) => {
   return (
-    <div className="image-block">
-      <div className="image-wrapper">
-        <img src={imageUrl} alt="Image" />
+    <div className='image-block'>
+      <div className='image-wrapper'>
+        <img src={imageUrl} alt='Image' className='custom-image' />
       </div>
-      <div className="text-wrapper">
+      <div className='text-wrapper'>
         <p>text</p>
       </div>
     </div>
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from './Image';
-
+import mainImage from  '../image/main.png';
 export default function HomePage() {
+  
   return (
     <div>
-      <Image />
+      <Image imageUrl={mainImage} />
     </div>
   );
 }
