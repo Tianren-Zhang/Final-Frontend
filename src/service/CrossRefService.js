8afc3ec06a4d9fb15163c908911ce76884093878
ztrrrrr <84163642+ztrrrrr@users.noreply.github.com>
2024-03-28 16:40:59 -0400

    Before add title

--- src/service/CrossRefService.js
+++ src/service/CrossRefService.js
@@ -13,4 +13,20 @@ const fetchPaperData = async (doi) => {
   }
 };
 
-export { fetchPaperData };
+const fetchPaperByTitle = async (title) => {
+  const url = `https://api.crossref.org/works?query.title="${encodeURIComponent(title)}"`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error('Network response was not ok');
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching paper data:', error);
+    throw error; // Re-throw to handle it in the component
+  }
+};
+
+
+export { fetchPaperData, fetchPaperByTitle };
