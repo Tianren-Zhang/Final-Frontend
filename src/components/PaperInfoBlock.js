3ef7bfd4b11e26236de21715f7da4e266cd195e1
ztrrrrr <84163642+ztrrrrr@users.noreply.github.com>
2024-03-28 21:19:36 -0400

    Add references at right

--- src/components/PaperInfoBlock.js
+++ src/components/PaperInfoBlock.js
@@ -47,6 +47,9 @@ export default function AuthorVisualizationBlock({ doi }) {
   }
 
   const { message } = paperData;
+  const validReferences = message.reference
+    ? message.reference.filter((ref) => ref['article-title'])
+    : null;
 
   return (
     <div className='block-container'>
@@ -61,51 +64,73 @@ export default function AuthorVisualizationBlock({ doi }) {
             DOI: {message.DOI}
           </a>
         )}
-        {message.author && (
-          <>
-            <h3>Authors:</h3>
-            <ul>
-              {message.author.map((author, index) => (
-                <li key={index}>
-                  {author.given} {author.family} -{' '}
-                  <small>
-                    {author.affiliation?.map((aff) => aff.name).join(', ')}
-                  </small>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-        {message.published && (
-          <p>Published On: {message.published['date-parts']?.[0]?.join('-')}</p>
-        )}
-        {message.event && (
-          <p>
-            Conference: {message.event.name} in {message.event.location},{' '}
-            {message.event.start['date-parts']?.[0]?.join('-')} to{' '}
-            {message.event.end['date-parts']?.[0]?.join('-')}
-          </p>
-        )}
-        <div className='actions-row'>
-          {message.link?.[0]?.URL && (
-            <a
-              href={message.link[0].URL}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Access Paper
-            </a>
-          )}
-          {message.license?.[0]?.URL && (
-            <a
-              href={message.license[0].URL}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              License
-            </a>
-          )}
-          <span>References: {message.reference?.length || 0}</span>
+
+        <div className='body-infoPage'>
+          <div className='left-infoPage'>
+            {message.author && (
+              <>
+                <h3>Authors:</h3>
+                <ul>
+                  {message.author.map((author, index) => (
+                    <li key={index}>
+                      {author.given} {author.family} -{' '}
+                      <small>
+                        {author.sequence} -{' '}
+                        {author.affiliation?.map((aff) => aff.name).join(', ')}
+                      </small>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+            {message.published && (
+              <p>
+                Published On: {message.published['date-parts']?.[0]?.join('-')}
+              </p>
+            )}
+            {message.event && (
+              <p>
+                Conference: {message.event.name} in {message.event.location},{' '}
+                {message.event.start['date-parts']?.[0]?.join('-')} to{' '}
+                {message.event.end['date-parts']?.[0]?.join('-')}
+              </p>
+            )}
+            <div className='actions-row'>
+              {message.link?.[0]?.URL && (
+                <a
+                  href={message.link[0].URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Access Paper
+                </a>
+              )}
+              {message.license?.[0]?.URL && (
+                <a
+                  href={message.license[0].URL}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  License
+                </a>
+              )}
+              <span>References: {message.reference?.length || 0}</span>
+            </div>
+          </div>
+          <div className='right-infoPage'>
+            <h3>Part of References:</h3>
+            {validReferences?.length > 0 ? (
+              <div className='references-container'>
+                {validReferences.map((ref, index) => (
+                  <div key={index} className='reference'>
+                    {ref['article-title']}.
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <p>No references available.</p> // Displayed if validReferences is null or empty
+            )}
+          </div>
         </div>
       </div>
     </div>
